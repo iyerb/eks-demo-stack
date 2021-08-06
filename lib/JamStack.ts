4d594cd7d675a0f4ada0c19757da3d7ea1a27654
iyerb <iyerb@amazon.com>
2021-08-07 07:47:19 +0800

    Added user data in cdk code

--- lib/JamStack.ts
+++ lib/JamStack.ts
@@ -2,25 +2,36 @@ import * as ec2 from '@aws-cdk/aws-ec2'
 import * as iam from '@aws-cdk/aws-iam'
 import * as cdk from '@aws-cdk/core'
 import * as eks from '@aws-cdk/aws-eks';
-import {readFileSync} from 'fs';
+import { readFileSync } from 'fs';
 import { CfnRefElement } from '@aws-cdk/core';
 import { KeyPair } from 'cdk-ec2-key-pair';
 
 const LAB_KEYPAIR_NM = 'lab-key-pair';
 export class JamStack extends cdk.Stack {
-    
-    constructor(scope: cdk.Construct, id:string, props?: cdk.StackProps ){
-        super(scope,id,props)
 
-        const keyname = new cdk.CfnParameter(this,'KeyName', { 
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props)
+
+        const keyname = new cdk.CfnParameter(this, 'KeyName', {
             type: 'String',
-            default: LAB_KEYPAIR_NM })
+            default: LAB_KEYPAIR_NM
+        })
+
+        const region = new cdk.CfnParameter(this, 'AWS_REGION', {
+            type: 'String',
+            default: process.env.CDK_DEFAULT_REGION
+        })
+
+        const account = new cdk.CfnParameter(this, 'AWS_ACCOUNT', {
+            type: 'String',
+            default: process.env.CDK_DEFAULT_ACCOUNT
+        })
 
         //create vpc
-        const vpc = new ec2.Vpc(this, 'jam-cdk-vpc',{
+        const vpc = new ec2.Vpc(this, 'jam-cdk-vpc', {
             cidr: '10.0.0.0/16',
             natGateways: 1,
-            subnetConfiguration:[{
+            subnetConfiguration: [{
                 name: 'public', cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC
             }],
         })
@@ -41,7 +52,7 @@ export class JamStack extends cdk.Stack {
             ec2.Port.tcp(80),
             'allow HTTP traffic from anywhere',
         );
-      
+
         securityGroup.addIngressRule(
             ec2.Peer.anyIpv4(),
             ec2.Port.tcp(443),
@@ -51,9 +62,12 @@ export class JamStack extends cdk.Stack {
         const role = new iam.Role(this, 'webserver-role', {
             assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
             managedPolicies: [
-            iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'),
+                iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'),
             ],
         });
+
+        const userData = ec2.UserData.forLinux();
+        this.getUserData(userData);
 
         // 👇 create the EC2 Instance
         const ec2Instance = new ec2.Instance(this, 'ec2-instance', {
@@ -65,22 +79,60 @@ export class JamStack extends cdk.Stack {
             securityGroup: securityGroup,
             instanceType: ec2.InstanceType.of(
                 ec2.InstanceClass.T2,
-                ec2.InstanceSize.MICRO,
+                ec2.InstanceSize.LARGE,
             ),
             machineImage: new ec2.AmazonLinuxImage({
                 generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
             }),
-            keyName: 
+            userData: userData,
+            keyName:
                 "Ref: KeyName"
-        });       
-         // 👇 load contents of script
-        const userDataScript = readFileSync('./lib/user-data.sh', 'utf8');
+        });
+        // 👇 load contents of script
+        //const userDataScript = readFileSync('./lib/user-data.sh', 'utf8');
         // 👇 add the User Data script to the Instance
-        ec2Instance.addUserData(userDataScript);
+        // ec2Instance.addUserData(userDataScript);
         new cdk.CfnOutput(this, 'Key Name', { value: LAB_KEYPAIR_NM })
-        new cdk.CfnOutput(this, 'IP Address', { value: ec2Instance.instancePublicIp });    
+        new cdk.CfnOutput(this, 'IP Address', { value: ec2Instance.instancePublicIp });
         new cdk.CfnOutput(this, 'Download Key Command', { value: 'aws secretsmanager get-secret-value --secret-id ec2-ssh-key/cdk-keypair/private --query SecretString --output text > cdk-key.pem && chmod 400 cdk-key.pem' })
         new cdk.CfnOutput(this, 'ssh command', { value: 'ssh -i cdk-key.pem -o IdentitiesOnly=yes ec2-user@' + ec2Instance.instancePublicIp })
-        
+
+    }
+
+    private getUserData(userData: ec2.UserData) {
+        userData.addCommands('yum update -y');
+        userData.addCommands('yum update -y');
+        userData.addCommands('sudo su');
+
+        userData.addCommands('curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash');
+
+        userData.addCommands('. ~/.nvm/nvm.sh');
+
+        userData.addCommands('nvm install');
+
+        userData.addCommands('node -e "console.log("Running, Node.js " + process.version)"');
+
+        userData.addCommands('npm install -g aws-cdk');
+        userData.addCommands('npm install @aws-cdk/aws-eks@1.116.0');
+
+        userData.addCommands('cdk --version');
+
+        userData.addCommands('curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"');
+        userData.addCommands('sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl');
+        userData.addCommands('chmod +x kubectl');
+        userData.addCommands('mkdir -p ~/.local/bin/kubectl');
+        userData.addCommands('mv ./kubectl ~/.local/bin/kubectl');
+
+        userData.addCommands('kubectl version');
+
+        userData.addCommands('sudo yum install git -y');
+
+        userData.addCommands('git version');
+        userData.addCommands('mkdir jam');
+        userData.addCommands('cd jam');
+        userData.addCommands('git clone https://github.com/iyerb/eks-demo.git');
+        userData.addCommands('cd eks-demo');
+        userData.addCommands('cdk bootstrap aws://'+process.env.CDK_DEFAULT_ACCOUNT+
+        '/'+process.env.CDK_DEFAULT_REGION);
     }
 }
--- lib/user-data.sh
+++ lib/user-data.sh
@@ -3,11 +3,11 @@ yum update -y
 sudo su
 #Connect to your Linux instance as ec2-user using SSH.
 #Install node version manager (nvm) by typing the following at the command line.
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 #Activate nvm by typing the following at the command line.
 . ~/.nvm/nvm.sh
 #Use nvm to install the latest version of Node.js by typing the following at the command line.
-nvm install node
+nvm install
 #Test that Node.js is installed and running correctly by typing the following at the command line.
 node -e "console.log('Running Node.js ' + process.version)"
 #Install aws-cdk
@@ -16,13 +16,19 @@ npm install @aws-cdk/aws-eks@1.116.0
 #check cdk version
 cdk --version
 #install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mkdir -p ~/.local/bin/kubectl
+mv ./kubectl ~/.local/bin/kubectl
 #check kubectl version
-kubectl --version
+kubectl version
 #install git
 sudo yum install git -y
 #Check git version
 git version
 mkdir jam
-https://github.com/iyerb/eks-demo.git
+cd jam
+git clone https://github.com/iyerb/eks-demo.git
+cd eks-demo
 echo "Done"
