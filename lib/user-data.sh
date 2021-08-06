4a6ad8fa1edb57644c1e11ee580a07f65233a4d5
iyerb <iyerb@amazon.com>
2021-08-07 06:20:15 +0800

    updates

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -17,33 +17,12 @@ npm install @aws-cdk/aws-eks@1.116.0
 cdk --version
 #install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+#check kubectl version
+kubectl --version
 #install git
 sudo yum install git -y
 #Check git version
 git version
-#mkdir jam
-
-mkdir eks-demo
-cd eks-demo
-cdk init -l typescript
-cd lib
-vi eks-demo-stack.ts
-
-cdk bootstrap aws://337828849457/ap-southeast-1
-
-# ⏳  Bootstrapping environment aws://337828849457/ap-southeast-1...
-#CDKToolkit: creating CloudFormation changeset...
-# ✅  Environment aws://337828849457/ap-southeast-1 bootstrapped.
-
- ✅  EksDemoStack
-
-#Outputs:
-#EksDemoStack.ckdeksclusterConfigCommandE04DCF13 = aws eks update-kubeconfig --name ckdekscluster91BD07D9-b77a056c8b304c29894ed2409c09e062 --region ap-southeast-1 --ro
-#le-arn arn:aws:iam::337828849457:role/EksDemoStack-ckdeksclusterMastersRoleBE3D5109-4N9G1Z9I7KR4
-#EksDemoStack.ckdeksclusterGetTokenCommand5DD9E553 = aws eks get-token --cluster-name ckdekscluster91BD07D9-b77a056c8b304c29894ed2409c09e062 --region ap-southeast-1 --
-#role-arn arn:aws:iam::337828849457:role/EksDemoStack-ckdeksclusterMastersRoleBE3D5109-4N9G1Z9I7KR4
-
-
-
-
-echo "<h1>It worked</h1>" > /usr/share/nginx/html/index.html
+mkdir jam
+https://github.com/iyerb/eks-demo.git
+echo "Done"
